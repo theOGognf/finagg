5df62d6333f5fb30fae4de138bb00c633353bc56
andrew <akberger20@gmail.com>
2022-10-02 21:42:58 -0500

    happy with BEA API org

--- src/shark/bea.py
+++ src/shark/bea.py
@@ -1,12 +1,92 @@
 import json
 import os
-from typing import ClassVar
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from datetime import datetime
+from typing import ClassVar, Literal
 
 import pandas as pd
 import requests
+import requests_cache
+
+requests_cache.install_cache("bea_api", ignored_parameters=["UserId", "ResultFormat"])
 
 
-class BEAAPI:
+class ThrottleWatchdog:
+    @dataclass
+    class Request:
+        time: datetime
+
+        size: int
+
+        is_error: bool
+
+    @dataclass
+    class State:
+        api_key: str
+
+        requests: list["ThrottleWatchdog.Request"]
+
+        @property
+        def errors_per_minute(self) -> int:
+            ...
+
+        @property
+        def next_valid_request_dt(self) -> float:
+            ...
+
+        @property
+        def requests_per_minute(self) -> int:
+            ...
+
+        @property
+        def volume_per_minute(self) -> int:
+            ...
+
+
+throttle_watchdog = ThrottleWatchdog()
+
+
+class DatasetAPI(ABC):
+    @classmethod
+    @property
+    @abstractmethod
+    def DATASET(cls) -> str:
+        ...
+
+    @classmethod
+    @abstractmethod
+    def get(cls, *, api_key: None | str = None) -> pd.DataFrame:
+        ...
+
+    @classmethod
+    def get_parameter_list(cls, /, *, api_key: None | str = None) -> pd.DataFrame:
+        return API.get_parameter_list(cls.DATASET, api_key=api_key)
+
+    @classmethod
+    def get_parameter_values(
+        cls, param: str, /, *, api_key: None | str = None
+    ) -> pd.DataFrame:
+        return API.get_parameter_values(cls.DATASET, param, api_key=api_key)
+
+
+class GDPByIndustry(DatasetAPI):
+    DATASET: ClassVar[str] = "GdpByIndustry"
+
+    def get(
+        self,
+        table_id: str = "ALL",
+        freq: Literal["A", "Q"] = "Q",
+        industry: str = "ALL",
+        *,
+        api_key: None | str = None,
+    ) -> pd.DataFrame:
+        API.get()
+
+
+class API:
+    gdp_by_industry: ClassVar[type[GDPByIndustry]] = GDPByIndustry
+
     MAX_REQUESTS_PER_MINUTE: ClassVar[int] = 100
 
     MAX_VOLUME_PER_MINUTE: ClassVar[int] = 100e6
@@ -15,52 +95,39 @@ class BEAAPI:
 
     URL: ClassVar[str] = "https://apps.bea.gov/api/data"
 
-    def __init__(
-        self,
-        api_key: None | str = None,
-        avoid_throttle: bool = True,
-        retry: bool = True,
-        return_type: type[pd.DataFrame | list[dict]] = pd.DataFrame,
-    ) -> None:
-        self.api_key = api_key or os.environ.get("BEA_API_KEY", None)
-        if not self.api_key:
+    @classmethod
+    def get(
+        cls, params: dict, results_key: str, /, *, api_key: None | str = None
+    ) -> pd.DataFrame:
+        api_key = api_key or os.environ.get("BEA_API_KEY", None)
+        if not api_key:
             raise RuntimeError(
                 "No BEA API key found. "
                 "Pass the API key to the API directly, or "
                 "set the `BEA_API_KEY` environment variable."
             )
-        self.avoid_throttle = avoid_throttle
-        self.retry = retry
-        self.return_type = return_type
+        params.update({"UserID": api_key, "ResultFormat": "JSON"})
+        response = requests.get(cls.URL, params=params)
+        results = json.loads(response.content)["BEAAPI"]["Results"][results_key]
+        return pd.DataFrame(results)
 
-    def describe(self, dataset: str, param: str, /) -> pd.DataFrame | list[dict]:
-        """"""
+    @classmethod
+    def get_parameter_list(
+        cls, dataset: str, /, *, api_key: None | str = None
+    ) -> pd.DataFrame:
         params = {
-            "UserID": self.api_key,
-            "method": "GetParameterValues",
-            "datasetname": dataset,
-            "ParameterName": param,
-            "ResultFormat": "JSON",
+            "Method": "GetParameterList",
+            "DatasetName": dataset,
         }
-        response = requests.get(self.URL, params=params)
-        results = json.loads(response.content)["BEAAPI"]["Results"]["ParamValue"]
-        if self.return_type is pd.DataFrame:
-            results = pd.DataFrame(results)
-        return results
+        return cls.get(params, "Parameter", api_key=api_key)
 
-    def get(self) -> pd.DataFrame | list[dict]:
-        ...
-
-
-class describe(BEAAPI):
-    def gdp_by_industry(self, param: str, /) -> pd.DataFrame | dict | str:
-
-        r = requests.get(self.URL, params=params)
-        return r.content
-
-
-class get(BEAAPI):
-    def gdp_by_industry(
-        self,
-    ) -> pd.DataFrame | dict | str:
-        ...
+    @classmethod
+    def get_parameter_values(
+        cls, dataset: str, param: str, /, *, api_key: None | str = None
+    ) -> pd.DataFrame:
+        params = {
+            "Method": "GetParameterValues",
+            "DatasetName": dataset,
+            "ParameterName": param,
+        }
+        return cls.get(params, "ParamValue", api_key=api_key)
