f6894be4e736297d29a4994c37715d351272b333
andrew <akberger20@gmail.com>
2023-02-26 13:28:56 -0600

    added finagg root path install option

--- src/finagg/__main__.py
+++ src/finagg/__main__.py
@@ -1,10 +1,18 @@
 """Main CLI entry points."""
 
+import logging
 import multiprocessing as mp
+import os
+import pathlib
 
 import click
 
-from . import fred, fundam, indices, sec, yfinance
+from . import fred, fundam, indices, sec, utils, yfinance
+
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s | %(message)s", level=logging.INFO
+)
+logger = logging.getLogger(__name__)
 
 
 @click.group()
@@ -24,6 +32,21 @@ cli.add_command(yfinance._cli.entry_point, "yfinance")
     "and install the recommended datasets into the SQL database.",
 )
 @click.option(
+    "--raw",
+    "-r",
+    is_flag=True,
+    default=False,
+    help="Whether to install raw data (data directly from APIs as-is).",
+)
+@click.option(
+    "--all",
+    "-a",
+    "all_",
+    is_flag=True,
+    default=False,
+    help="Whether to install all defined tables (including all refined tables).",
+)
+@click.option(
     "--processes",
     "-n",
     type=int,
@@ -40,14 +63,44 @@ cli.add_command(yfinance._cli.entry_point, "yfinance")
 @click.pass_context
 def install(
     ctx: click.Context,
+    raw: bool = False,
+    all_: bool = False,
     processes: int = mp.cpu_count() - 1,
     verbose: bool = False,
 ) -> None:
-    ctx.invoke(fred._cli.install, all_=True, verbose=verbose)
-    ctx.invoke(indices._cli.install, all_=True)
-    ctx.invoke(sec._cli.install, all_=True, processes=processes, verbose=verbose)
-    ctx.invoke(yfinance._cli.install, all_=True, processes=processes, verbose=verbose)
-    ctx.invoke(fundam._cli.install, all_=True, processes=processes)
+    if "FINAGG_ROOT_PATH" not in os.environ:
+        entered_root_path = input(
+            "Enter the path to your finagg root directory. finagg data will "
+            "be written to /path/to/root/findata/.\n\n"
+            "Root path (hit ENTER to use your current working directory): "
+        ).strip()
+        if not entered_root_path:
+            root_path = pathlib.Path.cwd()
+        else:
+            root_path = pathlib.Path(entered_root_path).expanduser().resolve()
+        p = utils.setenv("FINAGG_ROOT_PATH", str(root_path))
+        logger.info(f"FINAGG_ROOT_PATH written to {p}")
+    else:
+        logger.info("FINAGG_ROOT_PATH found in the environment")
+
+    if all_ or raw:
+        ctx.invoke(fred._cli.install, raw=raw, all_=all_, verbose=verbose)
+        ctx.invoke(indices._cli.install, all_=all_)
+        ctx.invoke(
+            sec._cli.install, raw=raw, all_=all_, processes=processes, verbose=verbose
+        )
+        ctx.invoke(
+            yfinance._cli.install,
+            raw=raw,
+            all_=all_,
+            processes=processes,
+            verbose=verbose,
+        )
+        ctx.invoke(fundam._cli.install, raw=raw, all_=all_, processes=processes)
+    else:
+        logger.info(
+            "Skipping installation because no installation options are provided"
+        )
 
 
 def main() -> int:
