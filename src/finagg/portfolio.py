4691aa7558e4a69965a507f12d3a137a26ea871d
andrew <akberger20@gmail.com>
2023-03-08 20:15:08 -0600

    portfolio uses decimal, documented too

--- src/finagg/portfolio.py
+++ src/finagg/portfolio.py
@@ -1,5 +1,9 @@
-"""Definitions related to tracking an investment portfolio of cash and stocks."""
+"""Definitions related to tracking an investment portfolio of cash and stocks.
+Underlying arithmetic uses exact decimal representations for max precision.
 
+"""
+
+from decimal import Decimal
 from functools import total_ordering
 
 
@@ -13,20 +17,20 @@ class Position:
 
     """
 
-    #: Average dollar cost for each share in the position.
-    average_cost_basis: float
+    # Average dollar cost for each share in the position.
+    _average_cost_basis: Decimal
 
-    #: Total dollar cost for all shares in the position.
-    #: The amount of dollars or cash invested in this security.
-    cost_basis_total: float
+    # Total dollar cost for all shares in the position.
+    # The amount of dollars or cash invested in this security.
+    _cost_basis_total: Decimal
 
-    #: Current number of shares owned in the position.
-    quantity: float
+    # Current number of shares owned in the position.
+    _quantity: Decimal
 
     def __init__(self, cost: float, quantity: float, /) -> None:
-        self.cost_basis_total = cost * quantity
-        self.average_cost_basis = cost
-        self.quantity = quantity
+        self._average_cost_basis = Decimal(cost)
+        self._quantity = Decimal(quantity)
+        self._cost_basis_total = self._average_cost_basis * self._quantity
 
     def __eq__(self, __o: object) -> bool:
         """Compare the position's cost basis."""
@@ -37,9 +41,9 @@ class Position:
             )
 
         if isinstance(__o, Position):
-            return self.average_cost_basis == __o.average_cost_basis
+            return self._average_cost_basis == __o._average_cost_basis
 
-        return self.average_cost_basis == __o
+        return self._average_cost_basis == __o
 
     def __lt__(self, __o: object) -> bool:
         """Compare the position's cost basis."""
@@ -50,9 +54,22 @@ class Position:
             )
 
         if isinstance(__o, Position):
-            return self.average_cost_basis < __o.average_cost_basis
+            return self._average_cost_basis < __o._average_cost_basis
 
-        return self.average_cost_basis < __o
+        return self._average_cost_basis < __o
+
+    @property
+    def average_cost_basis(self) -> float:
+        """Average dollar cost for each share in the position."""
+        return float(self._average_cost_basis)
+
+    @property
+    def cost_basis_total(self) -> float:
+        """Total dollar cost for all shares in the position. The amount of
+        dollars or cash invested in this security.
+
+        """
+        return float(self._cost_basis_total)
 
     def buy(self, cost: float, quantity: float, /) -> float:
         """Buy ``quantity`` of the position for ``cost``.
@@ -64,11 +81,30 @@ class Position:
         Returns:
             Value of the bought position.
 
+        Examples:
+            >>> from finagg.portfolio import Position
+            >>> pos = Position(100.0, 1)
+            >>> pos.buy(50.0, 1)
+            50.0
+            >>> pos.cost_basis_total
+            150.0
+            >>> pos.average_cost_basis
+            75.0
+            >>> pos.quantity
+            2.0
+
         """
-        self.quantity += quantity
-        self.cost_basis_total += cost * quantity
-        self.average_cost_basis = self.cost_basis_total / self.quantity
-        return cost * quantity
+        exact_cost = Decimal(cost)
+        exact_quantity = Decimal(quantity)
+        self._quantity += exact_quantity
+        self._cost_basis_total = self._cost_basis_total + exact_cost * exact_quantity
+        self._average_cost_basis = self._cost_basis_total / self._quantity
+        return float(exact_cost * exact_quantity)
+
+    @property
+    def quantity(self) -> float:
+        """Current number of shares owned in the position."""
+        return float(self._quantity)
 
     def sell(self, cost: float, quantity: float, /) -> float:
         """Sell ``quantity`` of the position for ``cost``.
@@ -84,12 +120,26 @@ class Position:
             `ValueError`: If there aren't enough shares
                 to sell in the position.
 
+        Examples:
+            >>> from finagg.portfolio import Position
+            >>> pos = Position(100.0, 2)
+            >>> pos.sell(50.0, 1)
+            50.0
+            >>> pos.cost_basis_total
+            100.0
+            >>> pos.average_cost_basis
+            100.0
+            >>> pos.quantity
+            1.0
+
         """
-        if self.quantity < quantity:
+        exact_cost = Decimal(cost)
+        exact_quantity = Decimal(quantity)
+        if self._quantity < exact_quantity:
             raise ValueError("Invalid order - not enough shares.")
-        self.quantity -= quantity
-        self.cost_basis_total = self.average_cost_basis * self.quantity
-        return cost * quantity
+        self._quantity -= exact_quantity
+        self._cost_basis_total = self._average_cost_basis * self._quantity
+        return float(exact_cost * exact_quantity)
 
     def total_dollar_change(self, cost: float, /) -> float:
         """Compute the total dollar change relative to the average
@@ -101,8 +151,14 @@ class Position:
         Returns:
             Total dollar change in value.
 
+        Examples:
+            >>> from finagg.portfolio import Position
+            >>> pos = Position(100.0, 1)
+            >>> pos.total_dollar_change(50.0)
+            -50.0
+
         """
-        return (cost - self.average_cost_basis) * self.quantity
+        return float((Decimal(cost) - self._average_cost_basis) * self._quantity)
 
     def total_percent_change(self, cost: float, /) -> float:
         """Compute the total percent change relative to the average
@@ -115,8 +171,14 @@ class Position:
             Total percent change in value. Negative indicates loss
             in value, positive indicates gain in value.
 
+        Examples:
+            >>> from finagg.portfolio import Position
+            >>> pos = Position(100.0, 1)
+            >>> pos.total_percent_change(50.0)
+            -0.5
+
         """
-        return (cost / self.average_cost_basis) - 1
+        return float((Decimal(cost) / self._average_cost_basis) - 1)
 
 
 class Portfolio:
@@ -127,22 +189,22 @@ class Portfolio:
 
     """
 
-    #: Total liquid cash on-hand.
-    cash: float
+    # Total liquid cash on-hand.
+    _cash: Decimal
 
-    #: Total cash deposited since starting the portfolio.
-    deposits_total: float
+    # Total cash deposited since starting the portfolio.
+    _deposits_total: Decimal
 
     #: Existing positions for each security.
     positions: dict[str, Position]
 
-    #: Total cash withdrawn since starting the portfolio.
-    withdrawals_total: float
+    # Total cash withdrawn since starting the portfolio.
+    _withdrawals_total: Decimal
 
     def __init__(self, cash: float, /) -> None:
-        self.cash = cash
-        self.deposits_total = cash
-        self.withdrawals_total = 0
+        self._cash = Decimal(cash)
+        self._deposits_total = self._cash
+        self._withdrawals_total = Decimal(0)
         self.positions = {}
 
     def __contains__(self, symbol: str) -> bool:
@@ -158,6 +220,11 @@ class Portfolio:
 
         """
         return self.positions[symbol]
+
+    @property
+    def cash(self) -> float:
+        """Total liquid cash on-hand."""
+        return float(self._cash)
 
     def buy(self, symbol: str, cost: float, quantity: float, /) -> float:
         """Buy ``quantity`` of security with ``symbol`` for ``cost``.
@@ -175,14 +242,27 @@ class Portfolio:
             `ValueError`: If the portfolio doesn't have enough cash
                 to execute the buy order.
 
+        Examples:
+            >>> from finagg.portfolio import Portfolio
+            >>> port = Portfolio(1000.0)
+            >>> port.buy("AAPL", 100.0, 1)
+            100.0
+            >>> pos = port["AAPL"]
+            >>> pos.cost_basis_total
+            100.0
+            >>> pos.average_cost_basis
+            100.0
+            >>> pos.quantity
+            1.0
+
         """
-        current_value = cost * quantity
-        if self.cash < current_value:
+        current_value = Decimal(cost) * Decimal(quantity)
+        if self._cash < current_value:
             raise ValueError("Invalid order - not enough cash.")
-        self.cash -= current_value
+        self._cash -= current_value
         if symbol not in self.positions:
             self.positions[symbol] = Position(cost, quantity)
-            return current_value
+            return float(current_value)
         else:
             return self.positions[symbol].buy(cost, quantity)
 
@@ -196,9 +276,15 @@ class Portfolio:
             Total cash in the portfolio.
 
         """
-        self.cash += cash
-        self.deposits_total += cash
-        return self.cash
+        exact_cash = Decimal(cash)
+        self._cash += exact_cash
+        self._deposits_total += exact_cash
+        return float(self._cash)
+
+    @property
+    def deposits_total(self) -> float:
+        """Total cash deposited since starting the portfolio."""
+        return float(self._deposits_total)
 
     def sell(self, symbol: str, cost: float, quantity: float, /) -> float:
         """Sell ``quantity`` of security with `symbol` for ``cost``.
@@ -212,12 +298,27 @@ class Portfolio:
             Value of the symbol's sold position in the
             portfolio.
 
+        Examples:
+            >>> from finagg.portfolio import Portfolio
+            >>> port = Portfolio(1000.0)
+            >>> port.buy("AAPL", 100.0, 2)
+            200.0
+            >>> port.sell("AAPL", 50.0, 1)
+            50.0
+            >>> pos = port["AAPL"]
+            >>> pos.cost_basis_total
+            100.0
+            >>> pos.average_cost_basis
+            100.0
+            >>> pos.quantity
+            1.0
+
         """
         current_value = self.positions[symbol].sell(cost, quantity)
-        if not self.positions[symbol].quantity:
+        if not self.positions[symbol]._quantity:
             self.positions.pop(symbol)
-        self.cash += cost * quantity
-        return current_value
+        self._cash += Decimal(cost) * Decimal(quantity)
+        return float(current_value)
 
     def total_dollar_change(self, costs: dict[str, float], /) -> float:
         """Compute the total dollar change relative to the total
@@ -229,8 +330,16 @@ class Portfolio:
         Returns:
             Total dollar change in value.
 
+        Examples:
+            >>> from finagg.portfolio import Portfolio
+            >>> port = Portfolio(1000.0)
+            >>> port.buy("AAPL", 100.0, 1)
+            100.0
+            >>> port.total_dollar_change({"AAPL": 50.0})
+            -50.0
+
         """
-        return self.total_dollar_value(costs) - self.deposits_total
+        return float(Decimal(self.total_dollar_value(costs)) - self._deposits_total)
 
     def total_dollar_value(self, costs: dict[str, float], /) -> float:
         """Compute the total dollar value of the portfolio.
@@ -241,12 +350,20 @@ class Portfolio:
         Returns:
             Total dollar value.
 
+        Examples:
+            >>> from finagg.portfolio import Portfolio
+            >>> port = Portfolio(1000.0)
+            >>> port.buy("AAPL", 100.0, 1)
+            100.0
+            >>> port.total_dollar_value({"AAPL": 50.0})
+            950.0
+
         """
-        dollar_value_total = self.cash
+        dollar_value_total = self._cash
         for symbol, cost in costs.items():
             if symbol in self.positions:
-                dollar_value_total += cost * self.positions[symbol].quantity
-        return dollar_value_total
+                dollar_value_total += Decimal(cost) * self.positions[symbol]._quantity
+        return float(dollar_value_total)
 
     def total_percent_change(self, costs: dict[str, float], /) -> float:
         """Compute the total percent change relative to the total
@@ -259,8 +376,18 @@ class Portfolio:
             Total percent change in value. Negative indicates loss
             in value, positive indicates gain in value.
 
+        Examples:
+            >>> from finagg.portfolio import Portfolio
+            >>> port = Portfolio(1000.0)
+            >>> port.buy("AAPL", 100.0, 1)
+            100.0
+            >>> port.total_percent_change({"AAPL": 50.0})
+            -0.05
+
         """
-        return (self.total_dollar_value(costs) / self.deposits_total) - 1
+        return float(
+            (Decimal(self.total_dollar_value(costs)) / self._deposits_total) - 1
+        )
 
     def withdraw(self, cash: float, /) -> float:
         """Withdraw cash from the portfolio.
@@ -275,9 +402,21 @@ class Portfolio:
             `ValueError`: If the portfolio doesn't have at least ``cash``
                 liquid cash to withdraw.
 
+        Examples:
+            >>> from finagg.portfolio import Portfolio
+            >>> port = Portfolio(1000.0)
+            >>> port.withdraw(100.0)
+            900.0
+
         """
-        if self.cash < cash:
+        exact_cash = Decimal(cash)
+        if self._cash < exact_cash:
             raise ValueError("Not enough cash to withdraw.")
-        self.cash -= cash
-        self.withdrawals_total += cash
-        return self.cash
+        self._cash -= exact_cash
+        self._withdrawals_total += exact_cash
+        return float(self._cash)
+
+    @property
+    def withdraws_total(self) -> float:
+        """Total cash withdrawn since starting the portfolio."""
+        return float(self._withdrawals_total)
